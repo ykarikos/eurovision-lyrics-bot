bace56b10eaa87c07619f338df10233206a12758
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2017-05-20 01:01:35 +0200

    lein new app eurovision-lyrics-bot

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to eurovision-lyrics-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/eurovision_lyrics_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns eurovision-lyrics-bot.core-test
+  (:require [clojure.test :refer :all]
+            [eurovision-lyrics-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
