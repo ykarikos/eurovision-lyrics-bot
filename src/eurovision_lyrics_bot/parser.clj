e17c1cbf032cb231b8bbe22f107b4fc4c1507b4c
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2017-05-21 01:29:27 +0200

    find-element & parse different html structures

--- src/eurovision_lyrics_bot/parser.clj
+++ src/eurovision_lyrics_bot/parser.clj
@@ -10,16 +10,39 @@
        (map #(traverse % 2 2))
        (filter #(not= % " "))))
 
+(declare find-element)
+
+(defn- find-element-from-children
+  [children elem-keyword attr-name attr-value]
+  (if (empty? children)
+      []
+      (let [first-child-search (find-element (first children) elem-keyword attr-name attr-value)]
+        (if (empty? first-child-search)
+          (find-element-from-children (rest children) elem-keyword attr-name attr-value)
+          first-child-search))))
+
+(defn- find-element
+  "Find the first element with the elem-keyword and attr-name=attr-value from tree"
+  [tree elem-keyword attr-name attr-value]
+  (if-not (vector? tree)
+    []
+    (let [root-keyword (first tree)
+          root-attr-value (attr-name (second tree))
+          children (drop 2 tree)]
+      (if (and (= root-keyword elem-keyword)
+               (= root-attr-value attr-value))
+          tree
+          (find-element-from-children children elem-keyword attr-name attr-value)))))
+
 (defn parse-song
   [html]
-  (let [content (traverse html 3 2 6)
-        header (traverse content 3 2 2 3 2 2 2 2 2)
-        country (traverse header 4 2)
-        year (traverse header 2 2)
-        lyrics-table (traverse content 7 2)
+  (let [header (find-element html :div :class "infohead2")
+        country (traverse header 2 4 2)
+        year (traverse header 2 2 2)
+        lyrics-table (find-element html :table :id "lyrics-table")
         title (traverse lyrics-table 2 2 2 2)
         lyrics (get-lyrics lyrics-table)]
-        {:year year
-         :country country
-         :title title
-         :lyrics lyrics}))
+      {:year year
+       :country country
+       :title title
+       :lyrics lyrics}))
