cb5c83a0fb9bd7dc99f718ca739b05fbe2ada25d
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2017-05-22 22:09:02 +0200

    add main program to run with real input

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject eurovision-lyrics-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Twitter bot that sends eurovision song texts as tweets"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
+                 [clj-http "3.5.0"]
+                 [environ "1.0.0"]]
   :main ^:skip-aot eurovision-lyrics-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/eurovision_lyrics_bot/core.clj
+++ src/eurovision_lyrics_bot/core.clj
@@ -1,7 +1,18 @@
 (ns eurovision-lyrics-bot.core
-  (:gen-class))
+  (:gen-class)
+  (:require [environ.core :refer [env]]
+            [pl.danieljanus.tagsoup :as tagsoup]
+            [eurovision-lyrics-bot.parser :as parser]
+            [clj-http.client :as client]))
+
+(defn get-song []
+  (-> (env :url)
+    client/get
+    :body
+    tagsoup/parse-string
+    parser/parse-song))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (get-song)))
