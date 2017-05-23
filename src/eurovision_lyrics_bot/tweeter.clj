5b03de69c8e2bfe57418c002890cde754a5f2b90
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2017-05-24 00:24:07 +0200

    add year and flag to tweet

--- src/eurovision_lyrics_bot/tweeter.clj
+++ src/eurovision_lyrics_bot/tweeter.clj
@@ -1,7 +1,21 @@
 (ns eurovision-lyrics-bot.tweeter
   (:require [twitter.api.restful :as restful]
             [environ.core :refer [env]]
-            [twitter.oauth :as oauth]))
+            [twitter.oauth :as oauth]
+            [clojure.java.io :as io]))
+
+(defn- split-flag [f]
+  (rest (re-find #"(.+) ([^ *]+)$" f)))
+
+(def flags
+  (->> "flags.txt"
+    io/resource
+    slurp
+    clojure.string/split-lines
+    (map split-flag)
+    flatten
+    (apply array-map)))
+
 
 (def credentials
   (oauth/make-oauth-creds
@@ -10,18 +24,22 @@
     (env :access-token)
     (env :access-token-secret)))
 
-(defn- create-tweet
+(defn- get-lyrics
   ([lyrics]
-    (create-tweet lyrics ""))
+    (get-lyrics lyrics ""))
   ([lyrics tweet]
     (let [new-tweet (str tweet (first lyrics) "\n")
           new-tweet-len (count new-tweet)]
       (if (> new-tweet-len 140)
         tweet
-        (create-tweet (rest lyrics) new-tweet)))))
+        (get-lyrics (rest lyrics) new-tweet)))))
 
 (defn tweet [song]
-  (let [tweet (-> song :lyrics create-tweet)]
+  (let [lyrics (-> song :lyrics get-lyrics)
+        flag (flags (:country song))
+        tweet (str lyrics
+              (:year song) " "
+              flag)]
     (restful/statuses-update
       :oauth-creds credentials
       :params {:status tweet})
